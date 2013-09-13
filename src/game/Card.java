08445d242a65e70732e0ad8202502902f806474e
Thomas <tkajder@gmail.com>
2013-09-12 23:02:13 -0700

    Added Suit enum, added Suit to Card.java

--- src/game/Card.java
+++ src/game/Card.java
@@ -1,5 +1,5 @@
 package game;
 
 public class Card {
-
+	private Suit mySuit;
 }
--- src/test/DeckTest.java
+++ src/test/DeckTest.java
@@ -1,5 +1,14 @@
 package test;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class DeckTest {
+
+	@Test
+	public void initializeDeckTest() {
+		fail("Not yet implemented");
+	}
 
 }
