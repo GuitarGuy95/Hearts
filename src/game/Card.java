5e445f02a4515f16379a55c4c36b61f145efc55e
Thomas <tkajder@gmail.com>
2013-09-12 23:12:03 -0700

    Completed Card.java class.

--- src/game/Card.java
+++ src/game/Card.java
@@ -1,5 +1,23 @@
 package game;
 
 public class Card {
-	private Suit mySuit;
+	// Suit is an enum with values of HEARTS, SPADES, DIAMONDS, CLUBS
+	private Suit suit;
+	// Card number is an int where 1 is Ace, 2-10 is numbers, 11 is Jack, 12 is
+	// Queen, and 13 is King
+	private int cardNumber;
+
+	// A card must be initialized with suit and card number
+	public Card(Suit suit, int cardNumber) {
+		this.suit = suit;
+		this.cardNumber = cardNumber;
+	}
+
+	public Suit getSuit() {
+		return this.suit;
+	}
+
+	public int returnCardNumber() {
+		return this.cardNumber;
+	}
 }
